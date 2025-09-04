8a549c68d0faad771585d36713ce9c93afa5cf07
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-09-04 20:25:03 +0530

    created user schema

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,6 +1,11 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-  //..
+  firstName: varchar("first_name", { length: 55 }).notNull(),
+  lastName: varchar("last_name", { length: 55 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
 });
