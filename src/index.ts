e33de063b73445975fc8f936d44b0165eca16383
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-09-04 19:17:38 +0530

    added script to run the project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT ?? 80000;
+const PORT = process.env.PORT ?? 8000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: `Server is up and runnig at PORT ${PORT}`,
+  });
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
